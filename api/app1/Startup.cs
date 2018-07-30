62378af86f7b535c965733c393efb68d2ddbd8d3
Jonathan <38092600+herrj1@users.noreply.github.com>
2018-07-29 23:08:55 -0400

    Added app1 implementation
    dotnet core 2.1+

---
+++ api/app1/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependecyInjection;
+using TodoApi.Models;
+
+namespace app1
+{
+    public class Startup
+    {
+		public void ConfigureServices(IServiceCollection services){
+			services.AddDbContext<TodoContext>(opt => opt.UserInMemoryDatabase("TodoList"));
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+		}
+		
+		public void Configure(IApplicationBuilder app){
+			app.UseDefaultFiles();
+			app.UseStaticFiles();
+			app.UseMvc();
+		}
+    }
+}
