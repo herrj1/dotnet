87b76770c030ef0670470ffb6463a86fed811bc0
Jonathan <38092600+herrj1@users.noreply.github.com>
2019-01-08 23:26:52 -0500

    Update ValuesController.cs

--- api/app1/Controllers/ValuesController.cs
+++ api/app1/Controllers/ValuesController.cs
@@ -28,13 +28,11 @@ namespace app1.Controllers
         public void Post([FromBody]string value)
         {
         }
-
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
-
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
