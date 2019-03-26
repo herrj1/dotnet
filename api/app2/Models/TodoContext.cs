1df17ab0ba047886db5183fc3c379f50b5bf1490
Jonathan <38092600+herrj1@users.noreply.github.com>
2019-03-25 23:37:35 -0400

    Update TodoContext.cs

--- api/app2/Models/TodoContext.cs
+++ api/app2/Models/TodoContext.cs
@@ -1,5 +1,4 @@
 using Microsoft.EntityFrameworkCore;
-
 namespace TodoApi.Models{
 	public class TodoContext : DbContext{
 		public TodoContext(DbContextOptions<TodoContext> options): base(options){}
