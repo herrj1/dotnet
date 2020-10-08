9572f6934e1db58973ca3c72799ca9c85faa7796
Jonathan <38092600+herrj1@users.noreply.github.com>
2020-10-07 23:44:25 -0400

    Update Startup.cs

--- api/app2/Startup.cs
+++ api/app2/Startup.cs
@@ -3,8 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependecyInjection;
 using TodoApi.Models;
+
 namespace app1
-{public class Startup
+
+{
+public class Startup
     {
 		public void ConfigureServices(IServiceCollection services){
 			services.AddDbContext<TodoContext>(opt => opt.UserInMemoryDatabase("TodoLists"));
